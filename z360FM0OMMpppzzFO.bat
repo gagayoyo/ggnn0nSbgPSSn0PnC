0658c9a4d1bbcde87255b1b1e80c14c35304b44c
gagayoyo <85374316+gagayoyo@users.noreply.github.com>
2022-03-24 00:20:45 +0000

    Update z360FM0OMMpppzzFO.bat

--- z360FM0OMMpppzzFO.bat
+++ z360FM0OMMpppzzFO.bat
@@ -1,5 +1,4 @@
-ÿþ
-f
+@echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By MBAH GADGET" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
@@ -15,5 +14,6 @@ ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Success!
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+GETIP = curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
+tasklist | find /i "ngrok.exe" >Nul && $GETIP || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 ping -n 10 127.0.0.1 >nul
