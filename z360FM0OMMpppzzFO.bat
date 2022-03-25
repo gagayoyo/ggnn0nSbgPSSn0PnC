08014b1cb8bce9eafc4eb00acc69e0edddb9bb0d
gagayoyo <85374316+gagayoyo@users.noreply.github.com>
2022-03-25 20:55:40 +0000

    Update z360FM0OMMpppzzFO.bat

--- z360FM0OMMpppzzFO.bat
+++ z360FM0OMMpppzzFO.bat
@@ -14,8 +14,5 @@ ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Success!
 echo IP:
-for /f "tokens=1" %%t in ('curl -s localhost:4040/api/tunnels ^|jq -r .tunnels[0].public_url') do set IP=%%t
-for /f "tokens=1" %%s in ('python -c "import base64;m='%IP%';mb=m.encode('ascii');b64b=base64.b64encode(mb);bm= b64b.decode('ascii');print(bm)"') do set cntnt=%%s
-for /f "tokens=1" %%i in ('curl https://api.github.com/repos/gagayoyo/ZsWgzwWW2IzqIZ2Wz/contents/IP ^| jq -r .sha') do set sha=%%i
-echo %IP%
-curl -X PUT https://api.github.com/repos/gagayoyo/ZsWgzwWW2IzqIZ2Wz/contents/IP -H "Accept:application/vnd.github.v3+json" -H "Authorization:token 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" -d "{\"message\":\"Update IP\",\"content\":\"%cntnt%\",\"sha\":\"%sha%\"}"
+set "GETIP=curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url" 
+tasklist | find /i "ngrok.exe" >Nul && %GETIP% || echo "Failed to retreive NGROK authtoken - check again your authtoken" ping -n 10 127.0.0.1 >nul
